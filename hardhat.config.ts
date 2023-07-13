2b93bc0ab3c0aa9c05b6b2e0cd214aa1c3210dda
nishant <nishantthesingh@gmail.com>
2023-07-13 22:25:03 +0530

    added contract sizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  contractSizer: {
+    runOnCompile: true,
+    strict: true,
+  },
 };
 
 export default config;
